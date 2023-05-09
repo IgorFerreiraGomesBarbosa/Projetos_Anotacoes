3dde061b7e4fc9bdb801ffde11e5831aa032d783
Igor Ferreira <igorfgb2006@gmail.com>
2023-05-09 10:15:19 -0300

    (Segundo) Pequeno projeto em Python

---
+++ P2.py
@@ -0,0 +1,14 @@
+idade: int
+salario: float; altura: float
+genero: str; nome: str
+
+idade = 20
+salario = 2.500,62
+altura = 1.82
+genero = "M"
+nome = "Joao Guilherme"
+
+print (f"IDADE = {idade} ")
+print (f"ALTURA = {altura} ")
+print (f"GENERO = {genero} ")
+print (f"NOME = {nome} ")
