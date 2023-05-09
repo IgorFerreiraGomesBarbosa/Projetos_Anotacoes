da9b5544ebeb3a74bff6706de97f697d34decbda
Igor Ferreira <igorfgb2006@gmail.com>
2023-05-09 09:54:13 -0300

    Projeto simples em Python

--- P1.py
+++ P1.py
@@ -6,8 +6,8 @@ hinicial = int(input("Hora incial: "))
 hfinal = int(input("Hora final: "))
 
 if hinicial < hfinal:
-    duracoa = hfinal - hinicial
+     duracoa = hfinal - hinicial
 else:
-    duracao = 24 - (hinicial - hfinal)
+     duracao = 24 - (hinicial - hfinal)
 
 print (f"O JOGO DUROU {duracao} HORA(S)")
