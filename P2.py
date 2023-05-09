9a2f7f9b8766b4639d99bce47e1e7b7e92aa823a
Igor Ferreira <igorfgb2006@gmail.com>
2023-05-09 10:51:11 -0300

    Update P2.py

--- P2.py
+++ P2.py
@@ -3,7 +3,7 @@ salario: float; altura: float
 genero: str; nome: str
 
 idade = 20
-salario = 2.500,62
+salario = 2.500
 altura = 1.82
 genero = "M"
 nome = "Joao Guilherme"
@@ -12,3 +12,4 @@ print (f"IDADE = {idade} ")
 print (f"ALTURA = {altura} ")
 print (f"GENERO = {genero} ")
 print (f"NOME = {nome} ")
+print (f"SALARIO MESNSAL = {salario:.3f} ")
